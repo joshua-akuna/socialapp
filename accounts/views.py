ed6d4269aaea0da5597d4b79f30930bbe25d5b77
root <root@tarajack.>
2025-06-28 15:03:37 +0100

    Models created in models.py flag

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
 # Create your views here.
 def signup(request):
-    return render(request, 'accounts/signup.html', {})
+    if request.method == 'POST':
+        print(request.POST)
+        return redirect('blog:index')
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import UniqueConstraint
+from django.db.models.functions import Lower
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profiles')
+    created_at = models.DateTimeField(auto_now_add=True)
+    #username = models.CharField(max_length=50, null=False, blank=False)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30, unique=True, blank=False, null=False)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural='Categories'
+        constraints = [
+            UniqueConstraint(
+                Lower('name'),
+                name='category_name_case_insensitive_unique',
+                violation_error_message='Category already exists (case insensitive match)'
+            ),
+        ] 
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='posts')
+    title = models.CharField(max_length=100, blank=False, null=False)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    class Meta:
+        # ordering
+        ordering = ['-updated_at']
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering=['-created_at']
